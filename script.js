5c7f45a185a777610df7b03e4df177babaa86c42
Nil Font i Cuesta <nilfontcuesta@gmail.com>
2024-09-13 13:56:31 +0200

    fix text size

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const textElement = document.getElementById('text');
 const menuElement = document.getElementById('menu');
+
 const texts = [
-    "by Nil Font i Cuesta\n",
-    "Welcome to my website\n",
-    "under construction\n"
-];
+    { content: "Welcome", class: "large-text" },
+    { content: "web under construction", class: "normal-text" },
+    { content: "by Nil Font i Cuesta", class: "samll-text" }
+]
 const typingSpeed = 100; 
 
 let currentTextIndex = 0;
@@ -12,10 +13,18 @@ let currentCharIndex = 0;
 
 function typeText() {
     if (currentTextIndex < texts.length) {
-        if (currentCharIndex < texts[currentTextIndex].length) {
-            textElement.innerHTML += texts[currentTextIndex][currentCharIndex] === '\n' 
-                ? '<br>' 
-                : texts[currentTextIndex][currentCharIndex];
+        const currentLine = texts[currentTextIndex];
+
+        if (currentCharIndex === 0) {
+            const lineDiv = document.createElement('div');
+            lineDiv.classList.add(currentLine.class);
+            lineDiv.id = `line${currentTextIndex}`;
+            textElement.appendChild(lineDiv); 
+        }
+
+        const lineDiv = document.getElementById(`line${currentTextIndex}`);
+        if (currentCharIndex < currentLine.content.length) {
+            lineDiv.innerHTML += currentLine.content[currentCharIndex];
             currentCharIndex++;
             setTimeout(typeText, typingSpeed);
         } else {
@@ -24,7 +33,6 @@ function typeText() {
             setTimeout(typeText, typingSpeed);
         }
     } else {
-        // Show menu after all text is typed
         menuElement.classList.remove('hidden');
     }
 }
